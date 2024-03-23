33618812fc37e21c4093cbdcf9279e7f3b4e3a72
vujelmanish <162692677+vujelmanish@users.noreply.github.com>
2024-03-23 18:01:43 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <title>Welcome to G-97 Saving</title>
+      <link rel="stylesheet" href="css/bootstrap.min.css" />
+      <script src="https://ajax.com.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      
+  </head>
+  <body style="background-color:lightblue;">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">G-97 Saving</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto mb-0 mb-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Downloads</span></a>
+        </li>
+          <li class="nav-item active">
+          <a class="nav-link" href="#">Contact</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">About</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/saving/register/index.php">Register</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/saving/login/index.php">Login</span></a>
+        </li>
+      </ul>
+      
+    </div>
+  </nav>
+
+  </body>
+  </html>
